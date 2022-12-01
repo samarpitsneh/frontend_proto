80b1ba4f6ef6ecf9541a3da08c4c3e56695abef0
Sneh Samarpit <65952450+samarpitsneh@users.noreply.github.com>
2022-12-01 15:48:31 +0530

    added bg and structured files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './App.css';
+
+import { Features, Footer, Blog, Header, Possibilty, WhatGPT3} from './containers'
+import { CTA, Brand, Navbar} from './components'
 
 const App = () => {
   return (
-    <div>
-        <h1>gpt-3 </h1>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header/> 
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibilty />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
---
+++ src/containers/possibilty/Possibilty.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Possibilty() {
+  return (
+    <div>Possibilty</div>
+  )
+}
+
+export default Possibilty
