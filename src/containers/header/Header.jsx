c5dfaccde24080f4ee2f96dbb9193dfafc541cdc
Sneh Samarpit <65952450+samarpitsneh@users.noreply.github.com>
2022-12-10 13:30:20 +0530

    added contents of header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 function Header() {
   return (
-    <div>Header</div>
+    <div className='gpt3_header section'>
+      <div className='gpt3_header-content'>
+        <h1 className='gradient_text'>Let's build something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      </div>
+      <div className='gpt3_header-content_input'>
+        <input type="email" placeholder="Your Email Address"></input>
+        <button>get started</button>
+      </div>
+
+      <div className='gpt3_header-content_people'>
+        <img src={people}/>
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+      <div className='gpt3_header-image'>
+          <img src={ai} alt="AI" />
+      </div>
+    </div>
   )
 }
 
